e6b9e1ad45fd56fe6899bad774492b503f50acef
Lucidarr <160017485+Lucidarr@users.noreply.github.com>
2024-03-22 19:23:11 +0300

    Add files via upload

---
+++ data types.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Example of integer data type
+  int age = 25;
+
+  // Example of double data type
+  double temperature = 25.5;
+
+  // Example of string data type
+  String name = "Lucy Maina";
+
+  // Example of list data type (list of integers)
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Example of map data type (mapping names to ages)
+  Map<String, int> agesMap = {
+    "John": 30,
+    "Alice": 25,
+    "Bob": 35,
+  };
+
+  // Print statements to demonstrate the values of each data type
+  print("Integer: $age"); // Output: Integer: 25
+  print("Double: $temperature"); // Output: Double: 25.5
+  print("String: $name"); // Output: String: Lucy Maina
+  print("List: $numbers"); // Output: List: [1, 2, 3, 4, 5]
+  print("Map: $agesMap"); // Output: Map: {John: 30, Alice: 25, Bob: 35}
+}
